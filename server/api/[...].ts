8362c342a7ddd154255ef8c7c94869a1b80d7673
imcm7 <136838917+imcm7@users.noreply.github.com>
2023-10-01 12:19:48 +0300

    Update [...].ts

--- server/api/[...].ts
+++ server/api/[...].ts
@@ -21,13 +21,6 @@ const runtimeConfig = useRuntimeConfig();
 
 // Refer to Auth.js docs for more details
 export const authOptions: AuthConfig = {
-  // pages: {
-  //   signIn: '/auth/signin',
-  //   signOut: '/auth/signout',
-  //   error: '/auth/error', // Error code passed in query string as ?error=
-  //   verifyRequest: '/auth/verify-request', // (used for check email message)
-  //   newUser: '/auth/new-user' // New users will be directed here on first sign in (leave the property out if not of interest)
-  // }
   callbacks: {
     async signIn() { // signIn({ user, account, profile, email, credentials })
       return true;
