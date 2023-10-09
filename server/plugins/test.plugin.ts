382181f08f8ba867ea2b99a38ed3ecc2c0214417
imcm7 <136838917+imcm7@users.noreply.github.com>
2023-10-09 13:43:10 +0300

    Update test.plugin.ts

--- server/plugins/test.plugin.ts
+++ server/plugins/test.plugin.ts
@@ -7,8 +7,10 @@ import chalk from 'chalk';
 import consola from 'consola';
 import { NitroApp } from 'nitropack';
 import { defineNitroPlugin } from 'nitropack/dist/runtime/plugin';
+import { User } from './server/entities/user.entity';
 
 export default defineNitroPlugin((nitroApp: NitroApp) => {
+    console.log(Object.getOwnPropertyNames(User.prototype));
     consola.info('test', chalk.blue('test'), chalk.green('test'));
     consola.info(colors.gray('copied to clipboard]'), 'test');
     consola.info([colors.gray('copied to clipboard]')]);
